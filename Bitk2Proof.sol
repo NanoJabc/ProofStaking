6343c569230fe133f67518875f897758e4e9fc7c
NanoJabc <89439506+NanoJabc@users.noreply.github.com>
2022-09-17 11:54:21 -0300

    Update Bitk2Proof.sol

--- Bitk2Proof.sol
+++ Bitk2Proof.sol
@@ -588,10 +588,10 @@ pragma solidity ^0.8.4;
  *
  * This contract is only required for intermediate, library-like contracts.
  */
-abstract contract Context {
-    function _msgSender() internal view virtual returns (address) {
-        return msg.sender;
-    }
+//abstract contract Context {
+  //  function _msgSender() internal view virtual returns (address) {
+   //     return msg.sender;
+  //  }
 
     function _msgData() internal view virtual returns (bytes calldata) {
         return msg.data;
