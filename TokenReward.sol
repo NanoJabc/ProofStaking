d68c6cf327df4c22909a6d755f42df574ac8abc9
NanoJabc <89439506+NanoJabc@users.noreply.github.com>
2022-10-15 15:31:44 -0300

    Update TokenReward.sol

--- TokenReward.sol
+++ TokenReward.sol
@@ -2,18 +2,32 @@
 
 pragma solidity 0.8.4;
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+contract Probandosupply is ERC20, ERC20Burnable, Ownable {
+  using SafeMath for uint256;
 
-contract TokenReward is ERC20, ERC20Burnable, Ownable {
-
+  mapping(address => uint256) private _balances;
   mapping(address => bool) controllers;
-  
-  constructor() ERC20("TokenReward", "TRPR") { }
+
+  uint256 private _totalSupply;
+  uint256 private MAXSUP;
+  uint256 constant MAXIMUMSUPPLY=20000000*10**18;
+
+  constructor() ERC20("TokenReward", "STK2") { 
+      _mint(msg.sender, 100000 * 10 ** 18);
+
+  }
 
   function mint(address to, uint256 amount) external {
     require(controllers[msg.sender], "Only controllers can mint");
+    require((MAXSUP+amount)<=MAXIMUMSUPPLY,"Maximum supply has been reached");
+    _totalSupply = _totalSupply.add(amount);
+    MAXSUP=MAXSUP.add(amount);
+    _balances[to] = _balances[to].add(amount);
     _mint(to, amount);
   }
 
@@ -33,4 +47,13 @@ contract TokenReward is ERC20, ERC20Burnable, Ownable {
   function removeController(address controller) external onlyOwner {
     controllers[controller] = false;
   }
+  
+  function totalSupply() public override view returns (uint256) {
+    return _totalSupply;
+  }
+
+  function maxSupply() public  pure returns (uint256) {
+    return MAXIMUMSUPPLY;
+  }
+
 }
